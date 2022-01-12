8557328629c6273463b206e188f4786057d11b25
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-01-12 18:06:44 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,122 @@
+var header = document.getElementById('header');
+var titlenav = document.getElementById('title');
+var titlenavback = document.getElementById('title2');
+var nav = document.getElementById('navigation');
+var lista = document.getElementsByClassName('nav-item');
+var line = document.getElementsByClassName('colorchange');
+
+window.addEventListener('scroll', () => {
+    var scroll = window.scrollY
+    if (scroll > 10) {
+        header.style.backgroundColor = "#fff";
+        header.classList.add('headerScrol');
+        titlenav.style.color = "#000";
+        titlenav.style.display = "none";
+        titlenavback.style.color = "#000";
+        titlenavback.style.display = "block";
+        nav.style.padding = "25px 0";
+        nav.style.alignItems = "center";
+        for (var i = 0, len = lista.length; i < len; i++) {
+            lista[i].style.color = "#000";
+        }
+        for (var ii = 0, lenn = line.length; ii < lenn; ii++) {
+            line[ii].style.color = "#000";
+        }
+
+    } else {
+        header.style.backgroundColor = "transparent";
+        header.classList.remove('headerScrol')
+        titlenav.style.color = "#fff";
+        titlenavback.style.color = "#fff";
+        titlenav.style.display = "block";
+        titlenavback.style.display = "none";
+        nav.style.padding = "70px 0";
+        nav.style.alignItems = "start";
+        for (var i = 0, len = lista.length; i < len; i++) {
+            lista[i].style.color = "#fff";
+        }
+        for (var ii = 0, lenn = line.length; ii < lenn; ii++) {
+            line[ii].style.color = "#fff";
+        }
+    }
+})
+
+
+$(document).ready(function () {
+    $('.autoplay').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('.autoplay2').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3.6,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
